b08d71231bb3487aab57ad970672a252d84c40ff
LordCod3r <64400282+MaskaraMehul@users.noreply.github.com>
2024-03-01 18:04:55 +0530

    main.m

--- main.m
+++ main.m
@@ -5,31 +5,46 @@ y = sin(x);
 
 baseline = 0.5;
 c = y > baseline;
-
 y1 = y(c);
-x1 = asin(y1);
-x2 = -(x1 - pi);
-x3 = (x1 + 2*pi);
-x4 = -(x1-3*pi);
 
-plot(x,y)
-hold on
-plot([0 10], [baseline baseline], "r--")
-hold on
-plot(x1,y1, "Blue", LineWidth = 1)
-hold on
-plot(x2,y1, "Blue", LineWidth = 1)
-hold on
-plot(x3,y1, "Blue", LineWidth = 1)
-hold on
-plot(x4,y1, "Blue", LineWidth = 1)
+if y == sin(x)
+    x1 = asin(y1);
+    x2 = -(x1 - pi);
+    x3 = (x1 + 2*pi);
+    x4 = -(x1-3*pi);
+    
+    plot(x,y)
+    hold on
+    plot(x1,y1, "Blue", LineWidth = 1)
+    hold on
+    plot(x2,y1, "Blue", LineWidth = 1)
+    hold on
+    plot(x3,y1, "Blue", LineWidth = 1)
+    hold on
+    plot(x4,y1, "Blue", LineWidth = 1)
+elseif y == cos(x)
+    x1 = acos(y1);
+    x2 = -(x1 - 2*pi);
+    x3 = (x1 + 2*pi);
+    x4 = -(x1-3*pi);
+
+    plot(x,y)
+    hold on
+    plot(x1,y1, "Blue", LineWidth = 1)
+    hold on
+    plot(x2,y1, "Blue", LineWidth = 1)
+    hold on
+    plot(x3,y1, "Blue", LineWidth = 1)
+end    
 
 xlabel("x")
-ylabel("sin(x)")
-title("sin(x)")
+ylabel("sin(x)/cos(x)")
+title("sin(x)/cos(x)")
 grid on
+plot([0 10], [baseline baseline], "r--")
+legend("sin(x)/cos(x)", "baseline", "above sin(x)/cos(x)")
 
-legend("sinx", "baseline", "above sin(x)")
+
 disp("Percentage of values greater than" + " " + baseline + " = " + ...
     length(c(c == 1))*100/length(y) + "%")
 
